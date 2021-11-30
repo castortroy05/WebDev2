8bfafcef40c66ab0be614d3e46640a155e3c5926
castortroy05 <44813655+castortroy05@users.noreply.github.com>
2021-11-30 23:37:21 +0000

    cw

---
+++ cw/webcw/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { Route, Routes } from 'react-router-dom'
+
+// We will create these two pages in a moment
+
+import HomePage from './pages/HomePage'
+
+import UserPage from './pages/UserPage'
+
+export default function App() {
+
+  return (
+
+    <Routes>
+
+      <Route exact path='/' component={HomePage} />
+
+      <Route path='/:id' component={UserPage} />
+
+    </Routes>
+
+  )
+
+}
